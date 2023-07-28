614880b294da1266dcae3aabbb7360633fd65aed
epeltz33 <epeltz@optonline.net>
2023-07-28 16:30:52 -0400

    draft 1 for simple slackbot

--- app.py
+++ app.py
@@ -1,13 +1,48 @@
 import os
-from flask import Flask
+import random
 from slack import WebClient
+from flask import Flask
+from slackeventsapi import SlackEventAdapter
 
 app = Flask(__name__)
+slack_events_adapter = SlackEventAdapter(os.environ.get("SLACK_EVENTS_TOKEN"), "/slack/events", app)
+slack_web_client = WebClient(token=os.environ.get("SLACKBOT_TOKEN"))  # allows us to interact with slack client
 
-slack_web_client = WebClient(token=os.environ.get("SLACKBOT_TOKEN"))
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+MESSAGE_TEMPLATE = {
+    "type": "section",
+    "text": {
+        "type": "mrkdwn",  # markdown
+        "text": (
+            "Hello, I'm a bot! :tada:\n\n"
+            "*How can I help you?*"
+        ),
+    },
+}
+
+
+@slack_events_adapter.on("message")  # listens for messages
+def message(payload):
+    event = payload.get("event", {})
+
+    text = event.get("text")
+
+    if "flip a coin" in text.lower():
+        channel_id = event.get("channel")
+        random_int = random.randint(0,1)
+        if random_int == 0:
+            results = "Heads"
+        else:
+            results = "Tails"
+        message = f"Flipping a coin: {results}"
+
+        MESSAGE_TEMPLATE["text"]["text"] = message
+        message_to_send = {"channel": channel_id, "blocks": [MESSAGE_TEMPLATE]}
+
+        return slack_web_client.chat_postMessage(**message_to_send)
+
+
+
+
 
 
 if __name__ == '__main__':
